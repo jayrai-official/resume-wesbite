b9c0966ce7dbda2cb9f8be61aa123819fabe0b61
Jay <jayrai18windows@gmail.com>
2023-01-28 19:15:40 +0530

    second commit - admin panel

--- resume/core/admin.py
+++ resume/core/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from core.models import Contact
 
 # Register your models here.
+@admin.register(Contact)
+class ContactRegister(admin.ModelAdmin):
+    list_display=['id','name','email','mob_no','msg']
--- resume/core/views.py
+++ resume/core/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
+from core.models import Contact
 
 # Create your views here.
 def home(request):
     return render(request,'core/index.html',{'home':'active'})
 
 def contact(request):
+    if request.method=='POST':
+        fetched_name = request.POST['name']
+        fetched_email = request.POST['email']
+        fetched_mob_no = request.POST['phone-no']
+        fetched_msg = request.POST['msg']
+
+        register = Contact(name=fetched_name,email=fetched_email,mob_no=fetched_mob_no,msg=fetched_msg)
+        register.save()
+        print("Data Saved sucessfully")
+
     return render(request,'core/contact.html',{'contact':'active'})
